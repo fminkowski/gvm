442c9a1e8ae41ceb0aac97cc744073533796d046
Fred Minkowski <fminkowski@gmail.com>
2018-08-29 19:41:30 -0400

    add conditional jump tests

--- source/gvm/cpu/operation.d
+++ source/gvm/cpu/operation.d
@@ -698,6 +698,48 @@ class ConditionalJump : Operation {
 	}
 }
 
+@test("Conditional jump operation updates instruction pointer if conditional register is set")
+unittest {
+	auto func_ptr = 42;
+	auto func_def = new FuncDef("function", func_ptr);
+
+	auto stack = new Stack!ubyte();
+	FuncDef[] call_stack;
+	auto cpu = new Cpu(stack, call_stack);
+	cpu.write("cn", 1);
+
+	auto instr = Instruction(OpCommand.cjmp, Command(), Command());
+	instr.ptr = func_def.ptr;
+
+	auto conditional_jump = new ConditionalJump(cpu);
+	conditional_jump.exec(instr);
+
+	auto ip = cpu.read_instr_ptr;
+	areEqual(func_ptr, ip);
+}
+
+@test("Conditional jump operation does not update instruction pointer if conditional register is not set")
+unittest {
+	auto expected_func_ptr = 0;
+	auto func_ptr = 42;
+	auto func_def = new FuncDef("function", func_ptr);
+
+	auto stack = new Stack!ubyte();
+	FuncDef[] call_stack;
+	auto cpu = new Cpu(stack, call_stack);
+	cpu.write("cn", 0);
+	
+	auto instr = Instruction(OpCommand.cjmp, Command(), Command());
+	instr.ptr = func_def.ptr;
+
+	auto conditional_jump = new ConditionalJump(cpu);
+	conditional_jump.exec(instr);
+
+	auto ip = cpu.read_instr_ptr;
+	areEqual(expected_func_ptr, ip);
+}
+
+
 class Call : Operation {
 	private {
 		Cpu cpu;
